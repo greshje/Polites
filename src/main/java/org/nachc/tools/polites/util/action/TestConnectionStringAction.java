bba3d1fe775bf50d8a2ee96591647665d35864c8
greshje <greshje@gmail.com>
2025-07-27 23:34:27 -0400

    work in progress

--- src/main/java/org/nachc/tools/polites/util/action/TestConnectionStringAction.java
+++ src/main/java/org/nachc/tools/polites/util/action/TestConnectionStringAction.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import org.nachc.tools.polites.util.connection.PolitesConnectionFactory;
 import org.yaorma.database.Data;
 import org.yaorma.database.Database;
-import org.yaorma.database.Row;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,21 +14,36 @@ public class TestConnectionStringAction {
 	public static void exec(String dbmsType, String jdbcConnectionString) {
 		Connection conn = null;
 		try {
+			String msg = "";
+			msg += "\n\n";
+			msg += "JAVA VERSION: " + System.getProperty("java.version") + "\n";
+			msg += "JAVA RUNTIME: " + System.getProperty("java.runtime.version") + "\n";
+			msg += "\n\n";
+			log.info(msg);
 			log.info("Getting connection...");
 			conn = PolitesConnectionFactory.getConnection(jdbcConnectionString);
 			log.info("Got connection: \n" + conn);
-			String sqlString = getTestQuery(dbmsType);
-			Data data = Database.query(sqlString, conn);
-			Row row = data.get(0);
-			String testString = row.get("test");
-			log.info("Test String: " + testString);
+			testConnection(conn, dbmsType);
+			log.info("Done with connection test.");
 		} finally {
 			Database.close(conn);
 		}
 		log.info("Done testing connection.");
 	}
 	
-	public static String getTestQuery(String dbmsType) {
-		return "select 'hello world' as test";
+	public static void testConnection(Connection conn, String dbmsType) {
+		if("databricks".equals(dbmsType)) {
+			testDatabricksConnection(conn);
+		}
+	}
+	
+	private static void testDatabricksConnection(Connection conn) {
+		String sqlString;
+		sqlString = "select distinct catalog_name, schema_name, schema_owner from information_schema.schemata;";
+		Data data = Database.query(sqlString, conn);
+		log.info("Got " + data.size() + " rows...");
+		for(int i = 0;i < data.size();i++) {
+			
+		}
 	}
 }
--- src/test/java/org/nachc/tools/politesforsqlserver/util/action/TestConnectionStringActionManualTest.java
+++ src/test/java/org/nachc/tools/politesforsqlserver/util/action/TestConnectionStringActionManualTest.java
@@ -12,5 +12,10 @@ public class TestConnectionStringActionManualTest {
 	public void shouldGetConnection() {
 		// TestConnectionStringAction.exec("ms sqlserver", "jdbc:sqlserver://localhost:1433;databaseName=synthea_omop;encrypt=false;TrustServerCertificate=True;user=synthea_omop;password=Sneaker01");
 		// TestConnectionStringAction.exec("postgresql", "jdbc:postgresql://broadsea-atlasdb:5432/postgres?user=postgres&password=mypass");
+		String url = "";				
+		url += "jdbc:databricks://dbc-bac6f3c2-1e35.cloud.databricks.com:443/default;";
+		url += "ssl=1;AuthMech=3;httpPath=/sql/1.0/warehouses/4513cdef142f8c41;UID=token;PWD=dapie6bff005a93e59fea01bce21443e2862;";
+		log.info("\n" + url);
+		TestConnectionStringAction.exec("databricks", url);
 	}
 }

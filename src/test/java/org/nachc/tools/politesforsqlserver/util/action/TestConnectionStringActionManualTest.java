24925979fcfc52d47b3a3f9b46bb826419b61c52
greshje <greshje@gmail.com>
2025-07-27 23:55:46 -0400

    work in progress

--- src/test/java/org/nachc/tools/politesforsqlserver/util/action/TestConnectionStringActionManualTest.java
+++ src/test/java/org/nachc/tools/politesforsqlserver/util/action/TestConnectionStringActionManualTest.java
@@ -14,7 +14,7 @@ public class TestConnectionStringActionManualTest {
 		// TestConnectionStringAction.exec("postgresql", "jdbc:postgresql://broadsea-atlasdb:5432/postgres?user=postgres&password=mypass");
 		String url = "";				
 		url += "jdbc:databricks://dbc-bac6f3c2-1e35.cloud.databricks.com:443/default;";
-		url += "ssl=1;AuthMech=3;httpPath=/sql/1.0/warehouses/4513cdef142f8c41;UID=token;PWD=dapie6bff005a93e59fea01bce21443e2862;";
+		url += "ssl=1;AuthMech=3;httpPath=/sql/1.0/warehouses/4513cdef142f8c41;UID=token;PWD=<TOKEN>;";
 		log.info("\n" + url);
 		TestConnectionStringAction.exec("databricks", url);
 	}
